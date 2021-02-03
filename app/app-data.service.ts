430530246e889a22bd1b649e34e4a7326ec6f54a
sandyYadav11290 <yadavsandeep11290@gmail.com>
2021-02-03 13:29:10 -0700

    2 files modified

--- app/app-data.service.ts
+++ app/app-data.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Injectable()
 export class AppDataService {
   myData1 = new BehaviorSubject([1, 2, 3]);
   myData2 = new BehaviorSubject([10, 11, 12]);
 
-  myEvent = new BehaviorSubject(false).pipe(data => {
-    
-  });
+  myEvent = new BehaviorSubject(false);
+
+  constructor() {
+    this.myEvent.subscribe(data => {
+      if (data === true) {
+        this.myData1.next([22, 54, 52]);
+      }
+    });
+  }
 }
--- app/app.component.ts
+++ app/app.component.ts
@@ -21,7 +21,5 @@ export class AppComponent {
 
   myEvent$ = this._appDataService.myEvent;
 
-  private init$: Observable<any>;
-
   public ngOnInit() {}
 }
